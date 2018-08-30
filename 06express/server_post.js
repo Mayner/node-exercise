1c8d6e259aeffce7afcb041146efbec832a7a88d
mayner <meiningzhu@163.com>
2018-08-30 10:58:32 +0800

    add express post

---
+++ 06express/server_post.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const expressStatic = require('express-static');
+const bodyParser = require('body-parser');
+
+var server = express();
+server.listen(8080);
+
+var dbUser = {
+    'mayner': '123',
+    'zhangsan': '456',
+    'lisi': '666'
+};
+
+server.use(bodyParser.urlencoded({
+    extended: false,    // 扩展模式
+    limit: 2*1024*1024  // 限制2MB
+}));
+
+server.post('/login', function (req, res) {
+    const user = req.body['user'];
+    const password = req.body['password'];
+    if (dbUser[user] == null) {
+        res.send({ok: false, msg: '此用户不存在'});
+    } else if (dbUser[user] != password) {
+        res.send({ok: false, msg: '密码错误'});
+    } else {
+        res.send({ok: true, msg: '登录成功'});
+    }
+});
+
+server.use(expressStatic('./www'));
